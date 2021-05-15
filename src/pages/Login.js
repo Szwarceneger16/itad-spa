39d5655a24300c39a0e6d493fbb1a0374317e1d8
Nataniel-Antosik <57491794+Nataniel-Antosik@users.noreply.github.com>
2021-05-15 15:29:21 +0200

    Tłumaczenia dla nagłówka logowanie i rejestracja

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,15 @@
 import { Flex, Stack, Box, Heading } from '@chakra-ui/react';
 import LoginForm from '../components/auth/loginForm';
 import React, { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 
 export function Login() {
+const { t, i18n } = useTranslation(["common"]);
 
   return (
       <Stack spacing={8} mx="auto" w="full" maxW="md" py={12} px={6}>
         <Heading fontSize="3xl" textAlign="center">
-          Login
+          {t("common:login.title")}
         </Heading>
         <Box rounded="lg" bg="white" boxShadow="lg" p={{ base: 4, md: 8 }}>
           <Stack direction={ "column"} spacing="24px" align="center">
