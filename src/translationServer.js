afe785c641c7ded81ac0ec5b83ada5757a995c01
Szwarceneger16 <sg44522@zut.edu.pl>
2021-05-19 22:46:33 +0200

    Update translationServer.js

--- src/translationServer.js
+++ src/translationServer.js
@@ -6,18 +6,11 @@ const fs = require("fs");
 const path = require("path");
 const port = 3101;
 
-i18next.use(middleware.LanguageDetector).init({
-  preload: ["en", "pl"],
-});
+// i18next.use(middleware.LanguageDetector).init({
+//   preload: ["en", "pl"],
+// });
 
 var app = express();
-
-// app.use(
-//   "/getTranslation",
-//   express.static(path.join(__dirname, "api\\mocks\\translations"), {
-//     index: "*.json",
-//   })
-// );
 
 app.use(
   "/getTranslation",
@@ -27,33 +20,29 @@ app.use(
   })
 );
 
-app.use(
+/* app.use(
   middleware.handle(i18next, {
-    ignoreRoutes: ["/foo"], // or function(req, res, options, i18next) { /* return true to ignore */ }
-  })
+    ignoreRoutes: ["/foo"], 
 );
 
-// in your request handler
-// app.get("/getTranslation/*", (req, res) => {
-//   var lng = req.language; // 'de-CH'
-//   var lngs = req.languages; // ['de-CH', 'de', 'en']
-//   req.i18n.changeLanguage("en"); // will not load that!!! assert it was preloaded
-//   console.log(lng);
-//   console.log(lngs);
-//   console.log(req.params);
+in your request handler
+app.get("/getTranslation/*", (req, res) => {
+  var lng = req.language; // 'de-CH'
+  var lngs = req.languages; // ['de-CH', 'de', 'en']
+  req.i18n.changeLanguage("en"); // will not load that!!! assert it was preloaded
+  console.log(lng);
+  console.log(lngs);
+  console.log(req.params);
 
-//   try {
-//     if(fs.existsSync('file.txt')) {
-//         res.sendFile('');
-//     } else {
-//         res.sendStatus(404);
-//     }
-// } catch (err) {
-//     console.error(err);
-// }
+  try {
+    if(fs.existsSync('file.txt')) {
+        res.sendFile('');
+    } else {
+        res.sendStatus(404);
+    }
+} catch (err) {
+    console.error(err);
+}
+}); */
 
-// });
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
